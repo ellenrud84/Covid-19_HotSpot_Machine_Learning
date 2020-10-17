0ee45dae1e6ab6bc6b9bff2783b2fcae47899844
Ellenrud84 <ellenrud84@gmail.com>
2020-10-17 12:24:47 -0500

    modified event listeners.js

--- static/js/event_listeners.js
+++ static/js/event_listeners.js
@@ -1,3 +1,26 @@
-console.log('test of evnt list');
+link= '/Demographics'
+d3.json(link).then((Data)=>{
+    const data = Data;
+    const entries= Object.values(data);
+    console.log(entries.length);
+    let i;
+    for(i=0;i<entries.length;i++){
+        let county = entries[i].county_name;
+        console.log('county');
+        console.log(county);
+    
+        //  Set the dropdown button with all subject IDs
+        d3.select("#selDataset")
+            .selectAll("option")
+            .data(subjectID)
+            .enter()
+            .append("option")
+            .html(function(d) {
+                return d;
+            });
+    }
+
+
+});
 
 
