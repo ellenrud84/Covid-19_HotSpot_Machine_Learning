c1fc95493a72a764a2a19fc25fb641a098d0c893
trackrun82 <64228800+trackrun82@users.noreply.github.com>
2020-10-19 13:18:08 -0600

    updated app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ parsed_full = []
 for county in county_list:
     queries_cd.append(pd.read_sql(f"select county_name, a.fips_code, date, cases, deaths from county_daily_data a join county_demographics b on a.fips_code =b.fips_code where county_name ='{county}'",conn))
     #queries_full.append(pd.read_sql(f"select county_name, a.* from county_daily_data a join county_demographics b on a.fips_code =b.fips_code where county_name ='{county}'",conn))
-    queries_full.append(pd.read_sql(f"with box1 as(select county_name, a.* from county_daily_data a join county_demographics b on a.fips_code =b.fips_code where county_name = '{county}'), box2 as(select county_name, a.* from lstm_data a join county_demographics b on a.fips_code2 =b.fips_code where county_name = '{county}') select a.*, b.lstm_cases_predicted from box1 a join lstm_data b on a.fips_code =b.fips_code2 and a.date = b.date2",conn))
+    queries_full.append(pd.read_sql(f"with box1 as(select county_name, a.* from county_daily_data a join county_demographics b on a.fips_code =b.fips_code where county_name = '{county}'), box2 as(select county_name, a.* from model_daily_data a join county_demographics b on a.fips_code =b.fips_code where county_name = '{county}') select a.*, b.lstm_cases_predicted, b.lstm_cases_residuals, b.lstm_deaths_predicted, b.lstm_deaths_residuals, b.sarimax_cases_predicted, b.sarimax_cases_residuals, b.sarimax_cases_forecasted, b.sarimax_deaths_predicted, b.sarimax_deaths_residuals, b.sarimax_deaths_forecasted from box1 a join model_daily_data b on a.fips_code =b.fips_code and a.date = b.date",conn))
 
 
 # loop to jsonify cases and deaths
